dcbec5233328e2e23fe7b63c6c8fce8140f7cac8
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-10 16:29:37 +0600

    3rd commit

---
+++ array.js
@@ -0,0 +1,5 @@
+const friends = ["tasfiq" , "Mustaq" , "Jubaer" , "rimon"];
+for (i=0; i<friends.length ; i++){
+    console.log(friends[i]);
+    console.log(i);
+}
---
+++ loop.js
@@ -0,0 +1,13 @@
+const num =[1,2,3,4,5];
+const reverse =[];
+for (let i=0; i<num.length; i++){
+    console.log(num[i]);
+    reverse.unshift(num[i]);
+}
+console.log(reverse);
+
+
+const num1=[1,2,3,4,5];
+console.log(num1);
+num.reverse()
+console.log(num)
